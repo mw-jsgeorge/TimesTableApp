aae0d5995e26449687050192e798e998dba99be1
mw-jsgeorge <73989764+mw-jsgeorge@users.noreply.github.com>
2025-10-28 14:58:10 +0530

    Update buildfile.m

--- buildfile.m
+++ buildfile.m
@@ -18,6 +18,10 @@ plan("check") = CodeIssuesTask;
 
 plan("pcode") = PcodeTask("source","source");
 
+plan("coverage") = Task( ...
+    Description = "Generate decision coverage report", ...
+    Actions = @decisionCoverageTask);
+
 % plan("deploy").Dependencies = ["check" "test"];
 
 % Make the "test" task the default task in the plan
@@ -28,4 +32,21 @@ end
 function deployTask(~)
 % Deploy standalone application
 compiler.build.standaloneApplication('source/timestable.mlapp');
+end
+
+function decisionCoverageTask(~)
+    import matlab.unittest.TestSuite
+    import matlab.unittest.plugins.CodeCoveragePlugin
+    import matlab.unittest.plugins.codecoverage.CoverageReport
+    import matlab.unittest.plugins.codecoverage.CoverageMetrics
+    suite = TestSuite.fromFolder("tests", "IncludingSubfolders", true);
+    runner = matlab.unittest.TestRunner.withTextOutput;
+
+    plugin = CodeCoveragePlugin.forFolder("source", ...
+        "IncludingSubfolders", true, ...
+        "Producing", CoverageReport("coverage-report", ...
+            "Metric", CoverageMetrics.Decision));
+
+    runner.addPlugin(plugin);
+    runner.run(suite);
 end
